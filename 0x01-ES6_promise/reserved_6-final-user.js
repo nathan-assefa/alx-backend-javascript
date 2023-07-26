c3dcf51f6c000ec70202ee75c8a15ae35d6571a4
nathan-assefa <aasamnew@gmail.com>
2023-07-26 11:14:46 +0000

    promise

--- 0x01-ES6_promise/reserved_6-final-user.js
+++ 0x01-ES6_promise/reserved_6-final-user.js
@@ -2,5 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+  return Promise.allSettled([signUpPromise, uploadPromise]);
 }
