933ca428b3d6fd1474763eb66a7d97e4f091e3ea
nathan-assefa <aasamnew@gmail.com>
2023-07-26 11:41:32 +0000

    promise

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -3,5 +3,5 @@ export default function divideFunction(numerator, denominator) {
     throw Error('cannot divide by 0');
   }
   const result = numerator / denominator;
-  return result
+  return result;
 }
