4747dea1a454e0ca7490e2107a999454fa103ce4
nathan-assefa <aasamnew@gmail.com>
2023-08-01 07:07:46 +0000

    data_manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,7 +5,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
 
-  int8Array[position] = value,
+  int8Array[position] = value;
 
   return new DataView(buffer);
 }
