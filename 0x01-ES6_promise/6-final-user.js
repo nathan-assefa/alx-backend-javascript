050dd538e5771f6bf6bd3ead5405542c32971435
nathan-assefa <aasamnew@gmail.com>
2023-08-13 06:14:38 +0000

    promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = [];
 
-export default handleProfileSignup;
+  const pushResult = async (promise) => {
+    try {
+      const value = await promise;
+      results.push({ status: 'fulfilled', value });
+    } catch (error) {
+      results.push({ status: 'rejected', value: error.toString() });
+    }
+  };
+
+  await Promise.allSettled([
+    pushResult(signUpUser(firstName, lastName)),
+    pushResult(uploadPhoto(fileName)),
+  ]);
+
+  return results;
+}
