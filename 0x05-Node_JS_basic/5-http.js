86cd799e68abe57ec57a025712ea0610da1f4df0
nathan-assefa <aasamnew@gmail.com>
2023-09-19 09:30:56 +0000

    node

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,51 +1,46 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-// Define the countStudents function
 async function countStudents (filePath) {
-  try {
-    const fileContents = await fs.readFile(filePath, 'utf-8');
-    const lines = fileContents.trim().split('\n');
+  const fileContents = await fs.readFile(filePath, 'utf-8');
+  const lines = fileContents.trim().split('\n');
 
-    // Initialize data structures to store counts and student names
-    const fieldCounts = {};
-    let totalStudents = 0;
+  // Initialize data structures to store counts and student names
+  const fieldCounts = {};
+  let totalStudents = 0;
 
-    // Process each line of the file
-    for (const line of lines.slice(1)) {
-      const [, , , field] = line.split(',');
-      if (field) {
-        totalStudents++;
+  // Process each line of the file
+  for (const line of lines.slice(1)) {
+    const [, , , field] = line.split(',');
+    if (field) {
+      totalStudents++;
 
-        if (fieldCounts[field]) {
-          fieldCounts[field].count++;
-          fieldCounts[field].students.push(line.split(',')[0]);
-        } else {
-          fieldCounts[field] = {
-            count: 1,
-            students: [line.split(',')[0]]
-          };
-        }
+      if (fieldCounts[field]) {
+        fieldCounts[field].count++;
+        fieldCounts[field].students.push(line.split(',')[0]);
+      } else {
+        fieldCounts[field] = {
+          count: 1,
+          students: [line.split(',')[0]]
+        };
       }
     }
-
-    // Prepare the result string
-    const resultString = [];
-    resultString.push(`Number of students: ${totalStudents}`);
-
-    // Add the number of students in each field to the result
-    for (const field in fieldCounts) {
-      if (field !== 'field') {
-        const studentList = fieldCounts[field].students.join(', ');
-        resultString.push(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${studentList}`);
-      }
-    }
-
-    // Join the lines of the result string
-    return resultString.join('\n');
-  } catch (error) {
-    throw new Error('Cannot load the database');
   }
+
+  // Prepare the result string
+  const resultString = [];
+  resultString.push(`Number of students: ${totalStudents}`);
+
+  // Add the number of students in each field to the result
+  for (const field in fieldCounts) {
+    if (field !== 'field') {
+      const studentList = fieldCounts[field].students.join(', ');
+      resultString.push(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${studentList}`);
+    }
+  }
+
+  // Join the lines of the result string
+  return resultString.join('\n');
 }
 
 // Create an HTTP app
