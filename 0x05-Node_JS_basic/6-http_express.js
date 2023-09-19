e85cefea6d73ce7e3f9dc3bbf5edc627db8e8804
nathan-assefa <aasamnew@gmail.com>
2023-09-19 10:27:59 +0000

    node

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = 1245;
 
-app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
+
+const port = 1245;
 
 app.listen(port, () => {
 });
