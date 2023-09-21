cb7766426423ba696d827eb8e7dee331c95be1b3
nathan-assefa <aasamnew@gmail.com>
2023-09-21 06:20:26 +0000

    mocha

--- 0x06-unittests_in_js/resrved_5-payment.test.js
+++ 0x06-unittests_in_js/resrved_5-payment.test.js
@@ -6,21 +6,23 @@ const assert = require('assert');
 describe('sendPaymentRequestToApi', function () {
   let logSpy;
 
-  beforeEach(() => {
+  beforeEach(function () {
     logSpy = sinon.spy(console, 'log');
   });
 
-  afterEach(() => {
+  afterEach(function () {
     logSpy.restore();
   });
 
-  it('should log "The total is: 120" when called with 100 and 20', () => {
+  it('should log "The total is: 120" when called with 100 and 20', function () {
     sendPaymentRequestToApi(100, 20);
-    assert(logSpy.calledOnceWith(`The total is: ${120}`));
+
+    assert(logSpy.calledOnceWith('The total is: 120'));
   });
 
-  it('should log "The total is: 20" when called with 10 and 10', () => {
+  it('should log "The total is: 20" when called with 10 and 10', function () {
     sendPaymentRequestToApi(10, 10);
-    assert(logSpy.calledOnceWith(`The total is: ${20}`));
+
+    assert(logSpy.calledOnceWith('The total is: 20'));
   });
 });
